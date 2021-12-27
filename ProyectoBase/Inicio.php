ea07ce38f24577489f0bb384ca86c482844969aa
Danf1730 <96731493+Danf1730@users.noreply.github.com>
2021-12-27 11:07:45 -0400

    Add files via upload
    Agregar vistas de las demas opciones

--- ProyectoBase/Inicio.php
+++ ProyectoBase/Inicio.php
@@ -4,15 +4,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Aseguradora Ucan Contineltal  <a href="Sesion.php" style="text-decoration: none"><li>Iniciar sesion</li></a></title>
+	<title>Aseguradora UCAB Contineltal  </title>
 	<link rel="stylesheet" type="text/css" href="style_proyecto/proyecto.css">
 </head>
 
 <body>
-<?php
-	<div class="cabeza" aling="left">
 
-		<h1>Aseguradora Ucan Contineltal</h1>
+	<div class="cabeza">
+		<h1><a href="Inicio.php" style="text-decoration: none">Aseguradora UCAB Contineltal  <a href="Sesion.php" style="text-decoration: none"><li>Iniciar sesion</li></a></h1>
 	</div>
 
 	<div class="row">
@@ -20,12 +19,12 @@
 		<div class="col-3 col-s-3 menu">
 			<ul>
 				<a href="#" style="text-decoration: none"><li>Inicio</li></a>
-				<a href="Vida.php" style="text-decoration: none"><li>Sucursales</li></a>
-				<a href="poliza.php" style="text-decoration: none"><li>Seguros/Polizas
+				<a href="Sucursales.php" style="text-decoration: none"><li>Sucursales</li></a>
+				<li>Seguros/Polizas
                    <ul>
-						<li><a href="">Vida</a></li>
-						<li><a href="">Hogar</a></li>
-						<li><a href="">Vehiculo</a></li>
+						<li><a href="Vida.php" style="text-decoration: none">Vida</a></li>
+						<li><a href="Hogar.php" style="text-decoration: none">Hogar</a></li>
+						<li><a href="Vehiculo.php" style="text-decoration: none">Vehiculo</a></li>
                   </ul>
                 </li></a> 
 				<a href="Financiamiento.php" style="text-decoration: none"><li>Financiamiento</li></a>
@@ -43,6 +42,6 @@
 	<div class="footer">
 		<p>Todos los derchos reservados &copy; 2022</p>
 	</div>
-    ?>
+    
 </body>
 </html>
---
+++ ProyectoBase/Vehiculo.php
@@ -0,0 +1,23 @@
+<!-- Daniel Nuñez,Javier Rojas ,Ashly Colmenares -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Vehiculo</title>
+	<link rel="stylesheet" type="text/css" href="style_proyecto/proyecto.css">
+</head>
+
+<body>
+
+	<div class="cabeza">
+		<h1><a href="Inicio.php" style="text-decoration: none">Aseguradora UCAB Contineltal </h1>
+	</div>
+
+	
+	<div class="footer">
+		<p>Todos los derchos reservados &copy; 2022</p>
+	</div>
+    
+</body>
+</html>
