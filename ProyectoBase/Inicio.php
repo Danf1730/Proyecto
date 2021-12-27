ad7125bfca0ea2bb8f399bf790e6db302e64b4b4
Danf1730 <96731493+Danf1730@users.noreply.github.com>
2021-12-27 09:55:23 -0400

    Add files via upload

---
+++ ProyectoBase/Inicio.php
@@ -0,0 +1,48 @@
+<!-- Daniel Nuñez,Javier Rojas ,Ashly Colmenares -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aseguradora Ucan Contineltal  <a href="Sesion.php" style="text-decoration: none"><li>Iniciar sesion</li></a></title>
+	<link rel="stylesheet" type="text/css" href="style_proyecto/proyecto.css">
+</head>
+
+<body>
+<?php
+	<div class="cabeza" aling="left">
+
+		<h1>Aseguradora Ucan Contineltal</h1>
+	</div>
+
+	<div class="row">
+
+		<div class="col-3 col-s-3 menu">
+			<ul>
+				<a href="#" style="text-decoration: none"><li>Inicio</li></a>
+				<a href="Vida.php" style="text-decoration: none"><li>Sucursales</li></a>
+				<a href="poliza.php" style="text-decoration: none"><li>Seguros/Polizas
+                   <ul>
+						<li><a href="">Vida</a></li>
+						<li><a href="">Hogar</a></li>
+						<li><a href="">Vehiculo</a></li>
+                  </ul>
+                </li></a> 
+				<a href="Financiamiento.php" style="text-decoration: none"><li>Financiamiento</li></a>
+			</ul>
+		</div>
+	
+		<div class="col-6 col-s-9">
+			
+			<div align="center"> <img src="img/ucab.jpg" width="400" height="400" alt="Imagen no disponible"> </div>
+			
+		</div>
+		
+	</div>
+	
+	<div class="footer">
+		<p>Todos los derchos reservados &copy; 2022</p>
+	</div>
+    ?>
+</body>
+</html>
