be85a780ef0438dce0b86c6373aa83b58b5f7c28
Danf1730 <danf1720@gmail.com>
2022-02-04 09:27:46 -0400

    Añadir Contrato vida

--- ProyectoBase/AñadirVida.php
+++ ProyectoBase/AñadirVida.php
@@ -4,9 +4,9 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Añadir Vida</title>
+	<title>Añadir vida </title>
 	<link rel="stylesheet" type="text/css" href="style_proyecto/añadir.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <nav class="navbar navbar-light" style="background-color: #2196F3;">
@@ -21,82 +21,71 @@
     <h1 class="mt-5"></h1>
 	<figure class="text-start ml-5">
   		<blockquote class="blockquote">
-			<p class="h3">Añadir póliza de Vida</p>
+      <p class="h3">Añadir Póliza de vida</p>
   		</blockquote>
 		<figcaption class="blockquote">Por favor ingrese los siguientes datos de la póliza</figcaption>
 	</figure>
-
+        
     <form action="insertarpolizaVidaBD.php" method="POST" class="w-30 p-3 border-0" style="background-color: white;">
-        <label for="">Id Póliza</label>
-        <input type="text" name="id_poliza" id="id_poliza" class="form-control mb-3" style="background-color: #E3F2FD;">
-        <div class="row g-3">
+          <div class="row g-3">
           <div class="col-auto">
-            <label for="" class="visually-hidden">Id Empleado Asegurador</label>
-            <input type="text" name="id_empleado" id="id_empleado" class="form-control mb-3" style="background-color: #E3F2FD;">
+          <label for="">Id Póliza</label>
+        <input type="text" name="nro_poliza" id="nro_poliza" class="form-control mb-3" style="background-color: #E3F2FD;">
+        </div>
+          <div class="col-auto">
+           <label for="" class="visually-hidden">Id Agente </label>
+            <input type="text" name="id_agente" id="id_agente" class="form-control mb-3" style="background-color: #E3F2FD;">
           </div>
           <div class="col-auto">
-            <label for="" class="visually-hidden">Id Cliente</label>
+          <label for="" class="visually-hidden">Id Cliente</label>
             <input type="text" name="id_cliente" id="id_cliente" class="form-control mb-3" style="background-color: #E3F2FD;">
           </div>
+          <div class="col-auto">
+          <label for="" class="visually-hidden">Id Beneficiario</label>
+            <input type="text" name="id_persona" id="id_persona" class="form-control mb-3" style="background-color: #E3F2FD;">
+            </div>
+          </div>
+       
         </div>
-        <label for="">Nombre del Cliente</label>
-        <input type="text" name="nombre_tomador" id="nombre_tomador" class="form-control mb-3" style="background-color: #E3F2FD;">
-        <label for="">Cobertura</label>
-        <input type="double" name="Cobertura" id="Cobertuta" class="form-control mb-3" style="background-color: #E3F2FD;">
-        <label for="">Forma de Pago</label>
-        <input type="text" name="forma_de_pago" id="forma_de_pago" class="form-control mb-3" style="background-color: #E3F2FD;">
+        
+        <label for="">Descripción</label>
+        <input type="text" name="descrip_poliza" id="descrip_poliza" class="form-control mb-3" style="background-color: #E3F2FD;">
+       
         <div class="row g-3">
           <div class="col-auto">
             <label for="" class="visually-hidden">Fecha del Contrato</label>
             <input type="date" name="f_contrato" id="f_contrato" class="form-control mb-3" style="background-color: #E3F2FD;">
           </div>
           <div class="col-auto">
-            <label for="" class="visually-hidden">Fecha de Renovación</label>
+          <label for="" class="visually-hidden">Fecha de Renovación</label>
             <input type="date" name="f_renovacion" id="f_renovacion" class="form-control mb-3" style="background-color: #E3F2FD;">
           </div>
         </div>
-        <label for="">Tipo</label>
-        <input type="text" name="tipo" id="tipo" class="form-control mb-3" style="background-color: #E3F2FD;" placeholder="Vida">
+         
         <div class="row g-3">
           <div class="col-auto">
-            <label for="" class="visually-hidden">Vigente</label>
-            <input type="text" name="vigente" id="vigente" class="form-control mb-3" placeholder="Si / No" style="background-color: #E3F2FD;">
+           <label for="" class="visually-hidden">Vigente</label>
+            <input type="text" name="estado_contrato" id="estado_contrato	" class="form-control mb-3" placeholder="Si / No" style="background-color: #E3F2FD;">
           </div>
+         
           <div class="col-auto">
-            <label for="" class="visually-hidden">Prima</label>
+           <label for="" class="visually-hidden">Prima</label>
             <input type="text" name="prima" id="prima" class="form-control mb-3" style="background-color: #E3F2FD;">
           </div>
         </div>
-        
-        <h1 class="mt-3"></h1>
-	    <figure class="text-start">
-		    <figcaption class="blockquote">Datos de Vida</figcaption>
-	    </figure>
-        <div class="row g-3">
-            <div class="col-auto">
-                <label for="" class="visually-hidden">Fecha de Nacimiento</label>
-                <input type="date" name="edad_cliente" id="edad_cliente" class="form-control mb-3" style="background-color: #E3F2FD;">
-            </div>
-            <div class="col-auto">
-                <label for="" class="visually-hidden">Cobertura Económica</label>
-                <input type="double" name="Cobertura2" id="Cobertuta2" class="form-control mb-3" style="background-color: #E3F2FD;">
-            </div>
-        </div>
-        <label for="">Profesión</label>
-        <input type="text" name="profesion_cliente" id="profesion_cliente" class="form-control mb-3" style="background-color: #E3F2FD;">
-       <input type="submit" name="añadir" class="mt-5">
+        <input type="submit" name="añadir" class="mt-5">
 </form>
 	
-    <nav class="navbar bottom navbar-light mt-5" style="background-color: #2196F3;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"> </a>
-	        <nav aria-label="Page navigation example">
-  		        <ul class="pagination justify-content-end">
-    		        <li class="page-item"><a class="page-link" href="MenuAgente.php">Regresar</a></li>
-  		        </ul>
-	        </nav>
-        </div>
-    </nav>
-    
+<nav class="navbar bottom navbar-light mt-5" style="background-color: #2196F3;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"> </a>
+	<nav aria-label="Page navigation example">
+  		<ul class="pagination justify-content-end">
+    		<li class="page-item"><a class="page-link" href="MenuAgente.php">Regresar</a></li>
+  		</ul>
+	</nav>
+  </div>
+</nav>
+
 </body>
 </html>
--- ProyectoBase/insertarpolizaVidaBD.php
+++ ProyectoBase/insertarpolizaVidaBD.php
@@ -1,119 +1,112 @@
 <?php
-include 'AñadirVida.php';
-       /////Datos Poliza//////
-       $id_poliza=$_POST['id_poliza'];  
-       $id_empleado=$_POST['id_empleado']; 
-       $nombre =$_POST['nombre_tomador'];
-       $cobertura=$_POST['Cobertura']; 
-       $pago=$_POST['forma_de_pago']; 
-       $contrato =$_POST['f_contrato'];
-       $renovacion =$_POST['f_renovacion']; 
-       $vigente=$_POST['vigente']; 
-       $tipo =$_POST['tipo'];
+      include 'AñadirHogar.php';
+       //// Datos Poliza(contrato)//////
+       $nro_poliza=$_POST['nro_poliza']; 
+       $descrip_poliza=$_POST['descrip_poliza'];
+       $contrato =$_POST['f_contrato']; 
+       $contrato2 =$_POST['f_renovacion']; 
        $prima =$_POST['prima']; 
-      ///////////////////////////
-      ///// Datos Vida//////
-      $id_poliza2=$_POST['id_poliza'];
-      $edad=$_POST['edad_cliente'];
-      $prof=$_POST['profesion_cliente'];
-      $cobertura2=$_POST['Cobertura2'];
-      $prima2=$_POST['prima']; 
-      /////////////////////
-        //Datos Propietario//  
-        $id_cliente=$_POST['id_cliente'];
-        $id_poliza3=$_POST['id_poliza'];
-        $id_empleado2=$_POST['id_empleado'];
-        $contrato2 =$_POST['f_contrato']; 
-        ////////////////////
+       /////////////////////////
+       //Datos Titular//  
+       $id_cliente=$_POST['id_cliente'];
+       $nro_poliza2=$_POST['nro_poliza'];
+       $prima2 =$_POST['prima'];
+       $contrato3=$_POST['f_contrato'];
+       ////////////////////
+       //// Datos vida //////
+       $id_vida=$_POST['nro_poliza'];
+       $prima3 =$_POST['prima'];
+       $descrip_poliza2=$_POST['descrip_poliza'];
+       //////////////////////
+       //// Datos Contrato //////
+       $id_vida2=$_POST['nro_poliza'];
+       $id_cliente2=$_POST['id_cliente'];
+       $id_agente=$_POST['id_agente'];
+       $id_persona=$_POST['id_persona'];
+       $contrato4=$_POST['f_contrato'];
+       $vigente=$_POST['estado_contrato'];
+       $prima4 =$_POST['prima'];
+       //////////////////////
+         
+
 
        try{
-              $base=new PDO("mysql:host=bcfwtrpmeo8khveqpqcu-mysql.services.clever-cloud.com:3306; dbname=bcfwtrpmeo8khveqpqcu" ,"uimjtch6xs9bod2v","fMfmvxQzfl8D6VWnmeCq");
+              $base=new PDO("mysql:host=bihmjsqmjuzv93yzuyjb-mysql.services.clever-cloud.com:3306; dbname=bihmjsqmjuzv93yzuyjb" ,"u6zp5irrvsbyntyd","YItp7ofnGTt8NbLiifkD");
               $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
               
-              if(strlen($_POST['id_poliza'])>=1 
-              && strlen($_POST['id_empleado'])>=1 
-              && strlen($_POST['nombre_tomador'])>=1 
-              && strlen($_POST['Cobertura'])>=1 
-              && strlen($_POST['forma_de_pago'])>=1 
+              if(strlen($_POST['nro_poliza'])>=1 
+              && strlen($_POST['descrip_poliza'])>=1 
               && strlen($_POST['f_contrato'])>=1 
               && strlen($_POST['f_renovacion'])>=1 
-              && strlen($_POST['vigente'])>=1 
-              && strlen($_POST['tipo'])>=1 
-              && strlen($_POST['prima'])>=1){
-////////// datos poliza///////////////
-              $sql="INSERT INTO  poliza (id_poliza,  
-                                         id_empleado, 
-                                         nombre_tomador,
-                                         cobertura, 
-                                         forma_de_pago, 
-                                         f_contrato,
-                                         f_Renovacion, 
-                                         vigente, 
-                                         tipo,
-                                         prima) 
-                                       
-                    VALUES (:id_poliza,
-                            :id_empleado,
-                            :nombre_tomador,
-                            :Cobertura,
-                            :forma_de_pago,
-                            :f_contrato,
-                            :f_renovacion,
-                            :vigente,
-                            :tipo,
-                            :prima)";
+              && strlen($_POST['prima'])>=1 
+              && strlen($_POST['id_cliente'])>=1 
+              && strlen($_POST['id_persona'])>=1
+              && strlen($_POST['id_agente'])>=1
+              && strlen($_POST['estado_contrato'])>=1
+              
+              ){
+       //////////// Datos Poliza(contrato)////////////////
+              $sql="INSERT INTO poliza(nro_poliza, descrip_poliza, f_contrato, f_renovacion, prima) 
+                    VALUES (:nro_poliza,:descrip_poliza,:f_contrato,:f_renovacion,:prima)";
                      
               $resultado=$base->prepare($sql);
-              $resultado->execute(array(":id_poliza"=>$id_poliza,
-                                        ":id_empleado"=>$id_empleado,
-                                        ":nombre_tomador"=>$nombre,
-                                        ":Cobertura"=>$cobertura,
-                                        ":forma_de_pago"=>$pago,
+              $resultado->execute(array(":nro_poliza"=>$nro_poliza,
+                                        ":descrip_poliza"=>$descrip_poliza,
                                         ":f_contrato"=>$contrato,
-                                        ":f_renovacion"=>$renovacion,
-                                        ":vigente"=>$vigente,
-                                        ":tipo"=>$tipo,
+                                        ":f_renovacion"=>$contrato2,
                                         ":prima"=>$prima));
-//////////////////////
-///poliza vida////////
-              $sql2="INSERT INTO poliza_vida ( id_poliza, edad_cliente, 
-                                              profesion_cliente, cobertura_economica, prima_asignada) 
-                            VALUES (:id_poliza,:edad_cliente,
-                            :profesion_cliente,:Cobertura2,:prima)";
-               $resultado2=$base->prepare($sql2);
-               $resultado2->execute(array(":id_poliza"=>$id_poliza2,
-                                         ":edad_cliente"=>$edad, 
-                                         ":profesion_cliente"=>$prof,
-                                         ":Cobertura2"=>$cobertura2,
-                                         ":prima"=>$prima2));
-                //Datos Propietario// 
-                $sql3="INSERT INTO propietario_poliza ( id_cliente, id_poliza, id_agente_personal, f_ultimo_uso) 
-                VALUES (:id_cliente,:id_poliza,:id_empleado,:f_contrato)";
+        //////////////////////////////////////////////
+         //////////// Datos Titular////////////////
+         $sql2="INSERT INTO titular(id_cliente, nro_poliza, saldo_prima, f_uso_reciente) 
+               VALUES (:id_cliente,:nro_poliza,:prima,:f_contrato)";
+                     
+         $resultado2=$base->prepare($sql2);
+         $resultado2->execute(array(":id_cliente"=>$id_cliente,
+                                    ":nro_poliza"=>$nro_poliza2,
+                                    ":prima"=>$prima2,
+                                    ":f_contrato"=>$contrato3));
+               //////////////////////////////////////////////
+                   //////////// Datos vida////////////////
+                      $sql3="INSERT INTO vida(id_vida_salud, prima, cobertura) 
+                             VALUES (:nro_poliza,:prima,:descrip_poliza)";
+                     
+                    $resultado3=$base->prepare($sql3);
+                      $resultado3->execute(array(":nro_poliza"=>$id_vida,
+                                                 ":prima"=>$prima3,
+                                                 ":descrip_poliza"=>$descrip_poliza2));
+                //////////////////////////////////////////////
+                  //////////// Datos Contrato ///////////////
+                      $sql4="INSERT INTO contrata_vida (id_vida_salud, id_cliente, id_persona, id_agente, f_contrato, estado_contrato, monto_comision_Ag) 
+                             VALUES (:nro_poliza,:id_cliente,:id_persona,:id_agente,:f_contrato,:estado_contrato,:prima)";
+                     
+                   $resultado4=$base->prepare($sql4);
+                    $resultado4->execute(array(":nro_poliza"=>$id_vida2,
+                                               ":id_cliente"=>$id_cliente2,
+                                               "id_persona"=>$id_persona,
+                                               ":f_contrato"=>$contrato4,
+                                               ":estado_contrato"=>$vigente,
+                                               ":id_agente"=>$id_agente,
+                                               ":prima"=>$prima3));
+             //////////////////////////////////////////////
+     
 
-$resultado3=$base->prepare($sql3);
-$resultado3->execute(array(":id_cliente"=>$id_cliente,
-                ":id_poliza"=>$id_poliza3,
-                ":id_empleado"=>$id_empleado2,
-                ":f_contrato"=>$contrato2,));           
-                //////////////////////       
-                                   
-                                   
-                                   
-                                         echo "Registrado correctamente";
-                                         
-                                   }else { echo "Error al registra, no pueden haber campos vacios"; 
+                                        echo "Registrado con exito:";  
+                                   }else { echo "Error al registar";   
                                    }
 
                                    
+                                   
+                                    
+
 
        }catch(Exception $e){
-             
        
               echo "mensaje error". $e->getMessage();
+              echo "----linea error". $e->getLine();
        }
 
        finally{
               $base=null;
               $base2=null;}
             
-    ?>       
+    ?>  
